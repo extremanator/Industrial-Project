ab679da8759f297f2b9e2df1e2fc0f5b9923b030
extremanator <49167307+extremanator@users.noreply.github.com>
2019-04-09 19:53:48 +0300

    Added files of "Hello World" code for Nodejs

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const http = require('http');
+const qs = require('querystring');
+const fs = require('fs');
+
+const port = 9000;
+
+function getHome(req, res){
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("<html><head><title>Home</title></head><body>Want to do some calculation? Click <a href='/calc'>here</a></body></html>");
+    res.end();
+}
+
+function get404(req, res){
+    res.writeHead(404, "Resource Not Found", {"Content-Type": "text/html"});
+    res.write("<html><head><title>404</title></head><body>404: Resource not found. Go to <a href='/'>home</a></body></html>");
+    res.end();
+}
+
+function get405(req, res){
+    res.writeHead(405, "Method Not Supported", {"Content-Type": "text/html"});
+    res.write("<html><head><title>405</title></head><body>405: Method not supported. Go to <a href='/'>home</a></body></html>");
+    res.end();
+}
+
+function getCalcForm(){
+
+}
+
+http.createServer(function (req, res) {
+    console.log(req.url);
+    switch (req.method){
+        case "GET":
+            if(req.url === "/"){
+                getHome(req, res);
+            } else if(req.url === '/calc'){
+                getCalcForm(req, res);
+            } else {
+                get404(req, res);
+            }
+            break;
+        case "POST":
+            break;
+        default:
+            get405(req, res);
+            break;
+    }
+}).listen(port);
